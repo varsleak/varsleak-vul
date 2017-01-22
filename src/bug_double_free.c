072ce498eb5f33ab2ec61a49cd87616a6e96bf7c
varsleak <varsleak@gmail.com>
2017-01-22 12:38:00 +0800

    modify c code

--- src/bug_double_free.c
+++ src/bug_double_free.c
@@ -10,37 +10,38 @@
 
 int main(int argc, const char * argv[])
 {
-    gdImagePtr im1 = 0;
+    gdImagePtr im1 = 0, im2 = 0;
     FILE *fp = 0;
     int size = 0;
 
-    char buf[512] = { 0 };
-
     if (argc != 2) {
 
-        getcwd(buf, sizeof(buf));
-        
-        printf("Usage: %s path/to/tests\n", buf);
+        printf("Usage: %s path/to/tests\n", argv[0]);
     }
 
-    //while(__AFL_LOOP(2000)){
+    fp = fopen(argv[1], "rb");
 
-        fp = fopen(argv[1], "rb");
-        if (fp){
-            
+    if (fp) {
+
+        while (__AFL_LOOP(2000))  {
+
+            fseek(fp, 0, SEEK_SET);
+
             im1 = gdImageCreateFromJpeg(fp);
-            fclose(fp);
 
-            if (im1){
-
-	            gdImageWebp(im1, "AAA.webp");
-
+            if (im1) {
+                im2 = gdImageWebpPtr(im1, &size);
+                if (im2){
+                   gdImageDestroy(im2); 
+                   im2 = 0;
+                }
                 gdImageDestroy(im1);
-
+                im1 = 0;
             }
-
         }
-    //}
+        fclose(fp);
+        fp = 0;
+    }
 
     return 0;
 }
